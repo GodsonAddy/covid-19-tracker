28a2b3fc15825be28b0f823c0e6c24ccf8923788
GodsonAddy <70777404+GodsonAddy@users.noreply.github.com>
2022-08-22 11:16:49 +0000

    responsive

--- src/components/QuickData.js
+++ src/components/QuickData.js
@@ -13,6 +13,45 @@ import { Container } from "@mui/system";
 function QuickData() {
   const { covidResults } = useSelector((state) => state.covid);
 
+  const lists = [
+    {
+      id: 1,
+      name: "COUNTRY",
+      topic: "NIGERIA",
+      color: "#4caf50",
+    },
+    {
+      id: 2,
+      name: "TOTAL SAMPLES TESTED",
+      topic: covidResults?.totalSamplesTested,
+      color: "#9c27b0",
+    },
+    {
+      id: 3,
+      name: "TOTAL CONFIRMED CASES",
+      topic: covidResults?.totalConfirmedCases,
+      color: "#ffc107",
+    },
+    {
+      id: 4,
+      name: "TOTAL ACTIVE CASES",
+      topic: covidResults?.totalActiveCases,
+      color: "#2196f3",
+    },
+    {
+      id: 5,
+      name: "TOTAL DISCHARGED",
+      topic: covidResults?.discharged,
+      color: "#00c853",
+    },
+    {
+      id: 6,
+      name: "TOTAL DEATH",
+      topic: covidResults?.death,
+      color: "#E60000",
+    },
+  ];
+
   return (
     <>
       <CssBaseline />
@@ -22,146 +61,37 @@ function QuickData() {
             container
             justifyContent="center"
             alignItems="center"
-            spacing={2}
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
           >
-            <Grid item xs={12} sm={4}>
-              <Card
-                raised
-                sx={{
-                  backgroundColor: "#4caf50",
-                  color: "white",
-                  borderRadius: 5,
-                  transition: "0.2s",
-                  "&:hover": {
-                    transform: "scale(1.1)",
-                  },
-                }}
-              >
-                <CardContent>
-                  <Typography variant="h5" sx={{ fontWeight: "bolder" }}>
-                    COUNTRY
-                  </Typography>
-                  <Typography variant="h3" sx={{ fontWeight: "bolder" }}>
-                    NIGERIA
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item sm={4} xs={12}>
-              <Card
-                raised
-                sx={{
-                  backgroundColor: "#9c27b0",
-                  color: "white",
-                  borderRadius: 5,
-                  transition: "0.2s",
-                  "&:hover": {
-                    transform: "scale(1.1)",
-                  },
-                }}
-              >
-                <CardContent>
-                  <Typography variant="h5" sx={{ fontWeight: "bolder" }}>
-                    TOTAL SAMPLES TESTED
-                  </Typography>
-                  <Typography variant="h3" sx={{ fontWeight: "bolder" }}>
-                    {covidResults?.totalSamplesTested}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item sm={4} xs={12}>
-              <Card
-                raised
-                sx={{
-                  backgroundColor: "#ffc107",
-                  color: "white",
-                  borderRadius: 5,
-                  transition: "0.2s",
-                  "&:hover": {
-                    transform: "scale(1.1)",
-                  },
-                }}
-              >
-                <CardContent>
-                  <Typography variant="h5" sx={{ fontWeight: "bolder" }}>
-                    TOTAL CONFIRMED CASES
-                  </Typography>
-                  <Typography variant="h3" sx={{ fontWeight: "bolder" }}>
-                    {covidResults?.totalConfirmedCases}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item sm={4} xs={12}>
-              <Card
-                raised
-                sx={{
-                  backgroundColor: "#2196f3",
-                  color: "white",
-                  borderRadius: 5,
-                  transition: "0.2s",
-                  "&:hover": {
-                    transform: "scale(1.1)",
-                  },
-                }}
-              >
-                <CardContent>
-                  <Typography variant="h5" sx={{ fontWeight: "bolder" }}>
-                    TOTAL ACTIVE CASES
-                  </Typography>
-                  <Typography variant="h3" sx={{ fontWeight: "bolder" }}>
-                    {covidResults?.totalActiveCases}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item sm={4} xs={12}>
-              <Card
-                raised
-                sx={{
-                  backgroundColor: "#00c853",
-                  color: "white",
-                  borderRadius: 5,
-                  transition: "0.2s",
-                  "&:hover": {
-                    transform: "scale(1.1)",
-                  },
-                }}
-              >
-                <CardContent>
-                  <Typography variant="h5" sx={{ fontWeight: "bolder" }}>
-                    TOTAL DISCHARGED
-                  </Typography>
-                  <Typography variant="h3" sx={{ fontWeight: "bolder" }}>
-                    {covidResults?.discharged}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item sm={4} xs={12}>
-              <Card
-                raised
-                sx={{
-                  backgroundColor: "#e60000",
-                  color: "white",
-                  borderRadius: 5,
-                  transition: "0.2s",
-                  "&:hover": {
-                    transform: "scale(1.1)",
-                  },
-                }}
-              >
-                <CardContent>
-                  <Typography variant="h5" sx={{ fontWeight: "bolder" }}>
-                    TOTAL DEATH
-                  </Typography>
-                  <Typography variant="h3" sx={{ fontWeight: "bolder" }}>
-                    {covidResults?.death}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
+            {/* LISTS */}
+
+            {lists?.map(({ id, color, topic, name }) => (
+              <Grid item key={id} xs={12} sm={4} md={6}>
+                <Card
+                  key={id}
+                  raised
+                  sx={{
+                    backgroundColor: color,
+                    color: "white",
+                    borderRadius: 5,
+                    transition: "0.2s",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
+                  }}
+                >
+                  <CardContent>
+                    <Typography variant="h5" sx={{ fontWeight: "bolder" }}>
+                      {name}
+                    </Typography>
+                    <Typography variant="h3" sx={{ fontWeight: "bolder" }}>
+                      {topic}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Box>
       </Container>
